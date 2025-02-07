43743ee6397ff36f2c3d200240d5fec03e860e68
kenotify <124643381+kenotify@users.noreply.github.com>
2025-02-07 14:54:38 +0800

    product.js-json

--- src/assets/Products/products.js
+++ src/assets/Products/products.js
@@ -1,14 +1,366 @@
 export const allExperience = [
-    {"projectTitle": "Cooperative Management Information System (CMIS)",
-     "projectDscription":"Description",
-     "modules": [
-        {
-         modulename:"",
-         content:[]
-        },
-    ]
-    },
-    {"projectTitle": "title",
-        "projectDscription":"Description"
-    }
-]
+  {
+    projectTitle: "Fleet Management and Dispatching Control System (eFMDSys)",
+    projectDscription:
+      "Designed to help the Department of Agriculture's General Suport Services Department(GSS). This include Vehicle Trip and Scheduling Management and Monitoring. With its sub module includes Vehicle and Driver Profiling and Fleet/Vehicle Repair and Preventive Maintenance",
+    modules: [
+      {
+        modulename: "Features",
+        content: [
+          "Fleet/Vehicle Listing",
+          "Vehicle Category, Model, Type, Make List",
+          "Vehicle Condition and Status",
+          "Driver List",
+          "Suppliers/Payee List",
+        ],
+      },
+      {
+        modulename: "Transactions",
+        content: [
+          "Vehicle Pre-Inspection Report",
+          "Vehicle Repair & Preventive Maintenance",
+          "Vehicle Waste Material Report",
+          "Vehicle-Fleet Dispatching Dashboard",
+          "Create Request for Vehicle Reservation",
+          "Confirm-Disapproved Reservation",
+          "Trip Ticket",
+          "Create Travel Order",
+        ],
+      },
+      {
+        modulename: "Reports",
+        content: [
+          "Vehicle Profiling Report",
+          "Vehicle Dispatching Report",
+          "Monthly Report of Travel",
+          "Waste Material Summary List",
+          "GSIS & LTO Expiration Report",
+        ],
+      },
+      {
+        modulename: "Travel Monitoring and Notifications",
+        content: [
+          "Dashboard Scheduled Travel Monitoring",
+          "Notifications, Updates and Travel Changes",
+        ],
+      },
+    ],
+  },
+  {
+    projectTitle: "Integrated Payroll Information System (iPIS)",
+    projectDscription: [
+      "Designed to cater to the demands of more than ten thousand (10,000) employees. This includes the first step of Payroll Computations, Daily Time Recording, Rate Set-up, Mandatory Deductions, Loans up to Printing of Individual PaySlip as well as Bank Transmittal Report.",
+      "It is a mixed platform interface, web-based and desktop application for a more robust, reliable, and scalable system.",
+    ],
+    modules: [
+      {
+        modulename: "Features",
+        content: [
+          "Employee Master File Entry Form",
+          "Rate and Compensation Entry with History",
+          "Mandatory and Other Deduction Entry",
+          "Payroll Generation and Reporting",
+          "Bank Transmittal Report",
+          "Direct Bank File Exporting for ATM and Non ATM",
+        ],
+      },
+      {
+        modulename: "Daily Time Record Module",
+        content: ["Uploading and Importing Biometric Data"],
+      },
+      {
+        modulename: "HR 201 File Module",
+        content: [
+          "Employee Record",
+          "Education and Eligibility Data",
+          "Trainings and Seminars Data",
+          "Applicants and Hired History",
+          "Demographic and Statistics Information",
+          "Sanctions, Violations, and Corporate Policy",
+          "Plantilla, Re-assignments, and Staffing",
+        ],
+      },
+      {
+        modulename: "Loans Module",
+        content: [
+          "Employee Ledger and Monitoring",
+          "Deductions Schedule and Payments",
+        ],
+      },
+      {
+        modulename: "Other Payments Computations",
+        content: [
+          "Last Pay / Resigned Pay Computation",
+          "Retirement Computation Pay",
+          "13th Month Pay Module",
+        ],
+      },
+    ],
+  },
+  {
+    projectTitle: "Procurement Document Filing System (PDFSys)",
+    projectDscription:
+      "Designed to ease the Procurement Documents and other related documents of Department of Agricultures Bids and Awards Committee (BAC A1, A2, and A3). With the scanned - electronic copy of all the documents in every Procurement Transactions this can help the end user work efficiency and daily throughput.",
+    modules: [
+      {
+        modulename: "Features",
+        content: [
+          "Entry of Documentary Requirement",
+          {
+            subcontent: [
+              {
+                name: "By Mode of Procurement",
+                options: [
+                  "Alternative Mode of Procurement (AMP)",
+                  "National Competitive Bidding (NCB)",
+                ],
+              },
+              {
+                name: "By Type",
+                options: ["INFRA", "GOODS", "SERVICES", "CONSULTANCY"],
+              },
+              "Mode of Procurement",
+              "Document Type",
+              "Supplier List",
+            ],
+          },
+        ],
+      },
+      {
+        modulename: "Transactions",
+        content: ["Manage New Document (Scan Document)", "Find Document"],
+      },
+      {
+        modulename: "Administrative",
+        content: ["User Management List", "Personnel and Designation List"],
+      },
+      {
+        modulename: "Archived Documents",
+        content: [
+          "Document Archiving for (10 Years above)",
+          "Find Archived Documents",
+        ],
+      },
+    ],
+  },
+  {
+    projectTitle: "ELECTRONIC PROPERTY INVENTORY SYSTEM (ePinSys)",
+    projectDscription: [
+      "Designed to give ease and solve the complexity of monitoring, tracking all the organizations Property, Plant and Equipment. Its key features include Unique Asset/Property Number Assignment, Direct Sticker Printing of Asset Number, Straight Depreciation Computation and Net Book Value and Standard to Custom made Monitoring and Reporting.",
+      "Its Report Print out and forms are based on the latest Government Accounting Form (GAM). The systems Interface and processes designed and consolidated according to different end users. It has a third party module using mobile devices for convenient Yearly Physical Count Inventory intended for multiple areas and office sites.",
+      "The module also includes the Archiving of Asset for Expired Items and Gate Pass Entry and Issuance.",
+    ],
+    modules: [
+      {
+        modulename: "feature",
+        content: [
+          "Entry of Article List and Classification",
+          "Article Brand, Unit of Measure and Type",
+          "Property Master List (By Status and Division, Office)",
+          "Funding Source List",
+          "Depreciation Monitoring and Net Book Value",
+          "List of Personnel Assigned",
+          "Suppliers List",
+          "Property Status",
+          "Fund Cluster List",
+          "Division- Office/OU's/Department List",
+          "Building, Room/Section/Cubicle List",
+        ],
+      },
+      {
+        modulename: "Transactions",
+        content: [
+          "Pre-Inspection Report",
+          "Repair and Preventive Maintenance",
+          "PAR/ICS Renewal",
+          "Property Transfer",
+          "Property update",
+          "Property, Plant & Equipment (PPE) Ledger Card",
+          "Biological Assets Property Card",
+        ],
+      },
+      {
+        modulename: "Reports",
+        content: [
+          "Report of Physical Count of Property, Plant and Equipment (RPCPPE)",
+          "Expired Small Items Inventory",
+          "Property Transfer Report",
+          "Waste Material Report",
+          "Report of Lost, Stolen, Damage or Destroyed Property (RLSDDP)",
+          "Report of Inventory & Inspection Report of Unserviceable Property (IIRUP)",
+          "Property Card Report",
+        ],
+      },
+      {
+        modulename: "Archiving",
+        content: [
+          "Yearly Schedule for Expired Small Items",
+          "Unserviceable Articles",
+        ],
+      },
+      {
+        modulename: "Gate Pass",
+        content: [
+          "Entry of Gate Pass Form",
+          "Monitoring of Borrowed Article/Property",
+        ],
+      },
+    ],
+  },
+  {
+    projectTitle: "ELECTRONIC PROCUREMENT SYSTEM (eProcSys)",
+    projectDscription: [
+      "Customized design in order to meet the demand and processes of the Philippine Republic Act R.A. 9184, known as 'An Act Providing for the Modernization, Standardization and Regulation of the Procurement Activities of the Government and for other Purposes,' otherwise known as the Government Procurement Reform Act.",
+      "Its report printouts and forms are based on the latest Government Accounting Form (GAM). The system's interface and processes are designed according to the flow of procurement (specifically for the Department of Agriculture), aiming to minimize the multiple encoding of PPMP, consolidation of APP reports, uniform item costing and pricing, procurement monitoring, APCPI requirements, and reports.",
+      "The module also includes inventory monitoring of office supplies and materials, covering issuance, monitoring, and its equivalent reports. Additionally, it provides templates for forms such as Disbursement Voucher, Obligation Request, and BURS Form.",
+    ],
+    modules: [
+      {
+        modulename: "Features",
+        content: [
+          "Entry of PPMP",
+          "Creation of Purchase Request (PR)",
+          "Creation of Agency Procurement Request (APR)",
+          "Request For Quotation (RFQ) Entry Form",
+          "Abstract of Quotation Entry Form",
+          "Abstract of Quotation Awarding",
+          "Create Purchase Order",
+          "Inspection and Acceptance Report",
+          "Create Job Order Form",
+          "Pre-Audit Report",
+        ],
+      },
+      {
+        modulename: "Templates for",
+        content: [
+          "Disbursement Voucher",
+          "Obligation Request",
+          "Budget Utilization Request and Status (BURS)",
+        ],
+      },
+      {
+        modulename: "Procurement Monitoring",
+        content: [
+          "AMP/NCB Monitoring for BAC A1, A2, A3",
+          "Deliverables",
+          "Procurement Monitoring Report",
+          "ACPCI Requirements",
+        ],
+      },
+      {
+        modulename: "Reports and Output",
+        content: [
+          "PPMP Report (By Division, OU's, Department)",
+          "PPMP Report - Revised",
+          "User Validation Report",
+          "BAC Validation Report",
+          "Program Coordinator Validation Report",
+          "Per Operating Unit Report",
+          "APP CSE Report",
+          "APP Non-CSE Report",
+          "APP BAC Verification Report",
+          "Per Expenditure Report",
+          "Item Master List Report",
+        ],
+      },
+      {
+        modulename: "Supplies Inventory Monitoring Module",
+        content: [
+          "Beginning Balance Entry",
+          "Receiving Report Entry",
+          "Requisition and Issuance Slip Entry",
+          "Electronic Stock Card",
+          "Report of Supplies and Materials Issued",
+          "Report on Physical Count of Inventories",
+        ],
+      },
+    ],
+  },
+  {
+    projectTitle: "Integrated Invnetory-Warehousing and Sale System (iIWSS)",
+    projectDscription: [
+      "Designed to monitor stocks receiving and issuances/sales. It can run on an integrated or stand-alone module. Customized reporting for Sales, Inventory, and Stocks Demographic.",
+    ],
+    modules: [
+      {
+        modulename: "Features",
+        content: [
+          "Product/Item Maintenance",
+          "Product Category and Classification",
+          "Set up for Beginning Balances",
+          "Receiving Report Entry",
+          "Withdrawals, Issuances and Transfers Entry",
+          "Adjustment and Returns Entry",
+          "Electronic Stock Card",
+          {
+            subcontent: [
+              {
+                contentname: "Reporting",
+                sub: [
+                  "Ending Inventory Count",
+                  "Re-Order Level Product Report",
+                  "Summary of Issuances, Withdrawals and Transfers",
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        modulename: "Sales Order Taking Module",
+        content: [
+          "Entry of Sales Transactions",
+          "Sales Return",
+          {
+            subcontent: [
+              {
+                contentname: "Reporting",
+                sub: [
+                  "Sales Summary Report",
+                  "Per Product Category Sales Report",
+                  "Top/Best Selling Items",
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        modulename: "Procurement Monitoring",
+        content: [
+          "AMP/NCB Monitoring for BAC A1, A2, A3",
+          "Deliverables",
+          "Procurement Monitoring Report",
+          "ACPCI Requirements",
+        ],
+      },
+      {
+        modulename: "Reports and Output",
+        content: [
+          "PPMP Report (By Division, OU's, Department)",
+          "PPMP Report - Revised",
+          "User Validation Report",
+          "BAC Validation Report",
+          "Program Coordinator Validation Report",
+          "Per Operating Unit Report",
+          "APP CSE Report",
+          "APP Non-CSE Report",
+          "APP BAC Verification Report",
+          "Per Expenditure Report",
+          "Item Master List Report",
+        ],
+      },
+      {
+        modulename: "Supplies Inventory Monitoring Module",
+        content: [
+          "Beginning Balance Entry",
+          "Receiving Report Entry",
+          "Requisition and Issuance Slip Entry",
+          "Electronic Stock Card",
+          "Report of Supplies and Materials Issued",
+          "Report on Physical Count of Inventories",
+        ],
+      },
+    ],
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,8 @@ export default {
       fontFamily:{
         tomorrow: ['Tomorrow', 'sans-serif'],
       }
-     
+
+    },
   },
   plugins: [],
 }
