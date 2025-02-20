5bea8963a776a571285d8e956b894b78d0963788
kenotify <124643381+kenotify@users.noreply.github.com>
2025-02-20 11:04:44 +0800

    Responsivesheesh

--- src/Components/ProductPortfolio.jsx
+++ src/Components/ProductPortfolio.jsx
@@ -29,22 +29,20 @@ export default function ProductPortfolio() {
     };
 
     return (
-        <div className="flex flex-col mt-10 m-5  mx-20 rounded-xl shadow-xl items-end bg-gradient-to-b from-blue-200 to-white" id="products-portfolio">
+        <div className="flex flex-col mx-4 md:mx-20 mt-10 m-5 rounded-xl shadow-xl items-end bg-gradient-to-b from-blue-200 to-white bg-PaperBlack" id="products-portfolio ">
             <div className="flex w-max self-start justify-start p-5 font-tomorrow flex-col ">
                 <div className='flex flex-row w-max gap-2 items-center mt-4'>
                     <NotebookText className='size-8 m-0 text-Font' />
-                    <h1 className="font-semibold text-4xl text-Font mt-0 m-5 mr-0 mb-2 ml-0">Product Portfolio</h1>
+                    <h1 className='font-semibold text-2xl md:text-4xl font-tomorrow text-Font m-5 mr-0 mb-2 mt-0 ml-0'>Product Portfolio</h1>
                 </div> 
                 <span className='border-b w-full border-2 ml-5 border-SCLBlue'></span>
             </div>
-            <div    
-                className="flex border border-black border-r-0 bg-gradient-to-b from-blue-200 to-white shadow-xl m-5 mr-0 mt-1 mb-14 rounded-tl-3xl p-5 w-11/12"
-            >
+            <div className='flex border border-black border-r-0 bg-gradient-to-b from-blue-200 to-white shadow-xl m-5 mr-0 mt-1 mb-14 rounded-tl-3xl p-5 w-full md:w-11/12 overflow-x-auto' ref={scrollRef} onWheel={handleScroll}> 
            
                 {content.map((con, i) => (
-                    <div key={i} className="flex flex-col items-center mx-5 ">
-                        <img src={con.icon} alt="Product" className="w-40 h-40 object-cover border border-black shadow-lg m-5 rounded-md hover:scale-110 transition duration-150 ease-in-out" />
-                        <h3 className="font-semibold text-center">{con.projectTitle}</h3>
+                    <div key={i} className="flex flex-col items-center min-w-[150px] md:min-w-[200px] ">
+                        <img src={con.icon} alt="Product" className="w-20 h-20 md:w-40 md:h-40 object-cover border border-black shadow-lg m-5 rounded-md hover:scale-110 transition duration-150 ease-in-out" />
+                        <h3 className="font-semibold text-center w-20 md:w-40">{con.projectTitle}</h3>
                     </div>
 
                 ))}
